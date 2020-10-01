980f50d66fadff486e013ece3b8ca619454dc530
sanchi0204 <sanchirastogi02@gmail.com>
2020-10-01 14:58:24 +0530

    Revert "added cardview"
    This reverts commit 5ea57aa3678fe18501556a19bd4e5d7d72ef2124.

--- app/src/main/java/com/noobsever/codingcontests/Screens/Settings.java
+++ app/src/main/java/com/noobsever/codingcontests/Screens/Settings.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.noobsever.codingcontests.R;
-
 public class Settings extends AppCompatActivity {
 
     @Override
